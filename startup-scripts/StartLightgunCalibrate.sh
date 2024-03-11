3afe29ee3dc105f5050c9c76571e4417d24c1aab
Michael DeGuzis <mdeguzis@gmail.com>
2024-03-11 18:15:29 -0400

    Add a bit of logging for CLI

--- startup-scripts/StartLightgunCalibrate.sh
+++ startup-scripts/StartLightgunCalibrate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "${HOME}/software/sinden"
 
+echo "[INFO] Calibrating Sinden lightgun"
+sleep 2
 mono-service LightgunMono.exe sdl steam joystick
 exit
 
--- startup-scripts/StartLightgunStart.sh
+++ startup-scripts/StartLightgunStart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "${HOME}/software/sinden"
 
+echo "[INFO] Starting Sinden lightgun"
+sleep 2
 mono-service LightgunMono.exe joystic
 exit
 
