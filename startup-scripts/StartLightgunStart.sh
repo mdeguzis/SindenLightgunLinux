52fd878a7722a6274c812a46a602db6645894549
Michael DeGuzis <mdeguzis@gmail.com>
2024-03-12 22:11:06 -0400

    logging

--- startup-scripts/StartLightgunStart.sh
+++ startup-scripts/StartLightgunStart.sh
@@ -11,10 +11,13 @@ main () {
 	sleep 2
 	echo "[INFO] Starting mono-service for Lightgun"
 	sudo mono-service LightgunMono.exe joystick
-	if [[ $? -ne 0 ]]; then
+	return_code=$?
+	if [[ ${return_code} -ne 0 ]]; then
 		echo "[ERROR] Command failed: 'mono-service LightgunMono.exe joystick'"
+	else
+		echo "[INFO] Sinden USB device successfully started at $(date)"
 	fi
-	echo "[INFO] Sinden USB device successfully started at $(date)"
+	echo "[INFO] Return code was ${return_code}"
 	exit
 }
 main 2>&1 | tee -a "/tmp/sinden-lightgun.log"
