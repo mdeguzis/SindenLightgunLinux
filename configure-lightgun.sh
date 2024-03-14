ba96707b482222bf839f03e4afde46028f00d14a
Steam Deck User <deck@steamdeck.localdomain>
2024-03-14 12:30:51 -0400

    Need to halt systemd service to run configuration

--- configure-lightgun.sh
+++ configure-lightgun.sh
@@ -5,7 +5,12 @@ set -e -o pipefail
 main() {
 	echo "[INFO] Configuring/calibrating lightgun"
 	cd /opt/sinden-lightgun
+	# Stop systemd service before starting this, as it conflicts with 
+	echo "[INFO] Halting systemd service to start configuration"
+	sudo systemctl stop sinden-lightgun.service
 	mono LightgunMono.exe steam joystick sdl
+	echo "[INFO] Starting systemd service for Sinden lightgun"
+	sudo systemctl start sinden-lightgun.service
 }
 main 2>&1 | tee -a "/tmp/sinden-lightgun.user.log"
 
